d9202ff4d4100d89028b78ec8eea23c081027a78
kgh2120 <kgh2120@gmail.com>
2025-02-14 17:55:45 +0900

    chore: CacheEvictMessage에 기본 생성자 추가

--- 6.cache-sync/src/main/java/org/kk/cachesync/dto/CacheEvictMessage.java
+++ 6.cache-sync/src/main/java/org/kk/cachesync/dto/CacheEvictMessage.java
@@ -2,7 +2,9 @@ package org.kk.cachesync.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class CacheEvictMessage {
