6ce188ee202f8ac4dc7556dde8ffc9971a23d086
kyoohyun <kgh2120@gmail.com>
2025-06-12 11:41:55 +0900

    test: Lock을 걸지 않는 상태에서의 수치 변화 테스트

--- 7.lock/src/main/java/com/kk/lock/service/AccountService.java
+++ 7.lock/src/main/java/com/kk/lock/service/AccountService.java
@@ -3,13 +3,16 @@ package com.kk.lock.service;
 import com.kk.lock.controller.BalanceResponse;
 import com.kk.lock.controller.SendRequest;
 import com.kk.lock.controller.TransactionResponse;
+import com.kk.lock.entity.Account;
 import com.kk.lock.repository.AccountRepository;
 import com.kk.lock.repository.TransactionLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class AccountService {
@@ -18,7 +21,17 @@ public class AccountService {
     private final TransactionLogRepository transactionLogRepository;
 
     public void send(SendRequest request){
+        Account sender = accountRepository.findById(request.senderAccountNumber())
+                .orElseThrow();
 
+        Account receiver = accountRepository.findById(request.receiverAccountNumber())
+                .orElseThrow();
+
+
+        long amount = request.amount();
+
+        sender.withdraw(amount);
+        receiver.deposit(amount);
     }
 
     public BalanceResponse getBalance(String accountNumber){
